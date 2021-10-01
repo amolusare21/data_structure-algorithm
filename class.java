25898166092e0e97ee978f99d644714717c71ead
amolusare21 <amolusare21@gmail.com>
2021-10-02 00:49:23 +0530

    First commit

---
+++ class.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Hellostudents{
+    public static void main(String[] args){
+
+        int a,b;
+      
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        a = sc.nextInt();
+        System.out.println("Enter the second number");
+        b = sc.nextInt();
+        int sum = a+b;
+        System.out.println("Sum of two numbers="+_sum);
+        sc = close();
+  }
+  }
